2b10476c858ee16e27f812bc523a28441a2bb63a
Viktoria Bayer <bayer.skaya@gmail.com>
2022-12-10 20:45:11 +0100

    one more pull request

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,38 @@
 import { TrendingFilmsApiService } from "./apiFilms/apiTrending";
 import axios from 'axios';
-<<<<<<< HEAD
-console.log('from main.js')
 const trending  = new TrendingFilmsApiService()
-trending.fetchFilms().then(response => console.log(response.results))
+
+// const releaseDate = new Date(2022-11-17);
+// const releaseYear = releaseDate.getFullYear()
+// console.log(releaseYear)
+
+const filmGallery = document.querySelector('.film-gallery')
+trending.fetchFilms().then(response => 
+    console.log(response.results)
+    // markUpGallery(response.results)
+    )
 trending.fetchGenres().then(response =>console.log(response))
 
-function markUpGallery(filmsArr, genres) {
-    return filmsArr
+function markUpGallery(filmsArr) {
+    
+    // return 
+    filmsArr
       .map(
         ({id, title, release_date, poster_path, genre_ids}) =>{
-            const genresList = genres.filter(genre => genre_ids.includes(genre.id))
-          `<div class = "gallery-item">
-          <a href="${item.largeImageURL}">
-          <img class="gallery-image" src="${item.webformatURL}" alt="${item.tags}" loading="lazy"/>
-          </a>
-          <div class="info">
-            <p class="info-item">
-              <b>Likes</b>${item.likes}
-            </p>
-            <p class="info-item">
-              <b>Views</b>${item.views}
-            </p>
-            <p class="info-item">
-              <b>Comments</b>${item.comments}
-            </p>
-            <p class="info-item">
-              <b>Downloads</b>${item.downloads}
-            </p>
+            const imgPath = `https://image.tmdb.org/t/p/w500${poster_path}`
+            // const genresList = genres.filter(genre => genre_ids.includes(genre.id))
+          const markup = `<li class = "gallery-item">
+           <img class="gallery-image" src="${imgPath}" alt="${title}" loading="lazy"/>
+           <div class="info">
+            <p class="info-item">${title}</p>
+            <p class="info-item">${genre_ids} | ${release_date}</p>
           </div>
-          </div>`}
+          </li>`}
       )
       .join('');
+      console.log(markup)
+      filmGallery.innerHTML = markup;
   }
   
-=======
-const trendingFilmsApiService = new TrendingFilmsApiService()
-trendingFilmsApiService.fetchFilms().then(response => console.log(response))
->>>>>>> 73ba0387242b39979eb9e6ffe8dca3ae55083b29
+
+
