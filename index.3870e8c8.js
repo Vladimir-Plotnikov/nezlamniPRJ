63ff3f5454c4668001e561c6203313435be22019
Vladimir-Plotnikov <71009993+Vladimir-Plotnikov@users.noreply.github.com>
2022-12-11 09:22:53 +0000

    Deploying to gh-pages from @ Vladimir-Plotnikov/nezlamniPRJ@febd56605d62dac2f28b521c61ddcefdd4b3d22e 🚀

--- index.3870e8c8.js
+++
@@ -1,2 +0,0 @@
-console.log("пагінація"),console.log("пагінація2"),console.log("пагінація3222");
-//# sourceMappingURL=index.3870e8c8.js.map
---
+++ index.8b6f28ef.js
@@ -0,0 +1,2 @@
+console.log("main"),console.log("пагінація"),console.log("пагінація2"),console.log("пагінація3222");
+//# sourceMappingURL=index.8b6f28ef.js.map
